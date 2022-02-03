bedefbf41702735aa258215bca8dc0af5cff9155
Soni Dharma Ardhana <58514579+01soni247@users.noreply.github.com>
2022-02-03 21:42:09 +0700

    Update buled.sh

--- buled.sh
+++ buled.sh
@@ -1,38 +1,62 @@
-#                    #
-# NEW KERNEL BUILDER #
-#                    #
-
-
+#!/usr/bin/env bash
 #
-# Toolchain Path
+# Copyright (C) 2021 a xyzprjkt property
 #
-ClangPath=$(pwd)/clang 
-KernelPath=$(pwd)/kernel 
 
+# Needed Secret Variable
+# KERNEL_NAME | Your kernel name
+# KERNEL_SOURCE | Your kernel link source
+# KERNEL_BRANCH  | Your needed kernel branch if needed with -b. eg -b eleven_eas
+# DEVICE_CODENAME | Your device codename
+# DEVICE_DEFCONFIG | Your device defconfig eg. lavender_defconfig
+# ANYKERNEL | Your Anykernel link repository
+# TG_TOKEN | Your telegram bot token
+# TG_CHAT_ID | Your telegram private ci chat id
+# BUILD_USER | Your username
+# BUILD_HOST | Your hostname
 
-# clone
-git clone --depth=1 https://github.com/Redmi-MT6768/android_kernel_xiaomi_mt6768 -b twelve $KernelPath
-git clone --depth=1 https://github.com/kdrag0n/proton-clang $ClangPath
+echo "Downloading few Dependecies . . ."
+# Kernel Sources
+git clone --depth=1 https://github.com/kdrag0n/proton-clang proton # proton set as Clang Default
 
-#
-# Main
-#
-export TZ="Asia/Jakarta"
-export KERNELNAME=JRE205-Kernel 
-export TG_CHAT_ID=1689573524 
-export TG_TOKEN=1689573524:AAHGMEDNkEpxSEc_c_rgwqaQ762n_dWJjJs
-export KBUILD_BUILD_USER=@DreamersGo
-export KBUILD_BUILD_HOST=ArmG80 
-export DATE=$(date "+%m%d")
-export HASH=$(git rev-parse --short HEAD)
+# Main Declaration
+KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_DEFCONFIG=lancelot_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
+DEVICE_CODENAME=Lancelot
+KERNEL_NAME=JRE205-ED101
+TG_TOKEN=1689573524:AAHGMEDNkEpxSEc_c_rgwqaQ762n_dWJjJs
+TG_CHAT_ID=1689573524
+CODENAME=lancelot
+ANYKERNEL=https://github.com/rama982/AnyKernel3
+CLANG_ROOTDIR=$(pwd)/proton # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER=Soni # Change with your own name or else.
+export KBUILD_BUILD_HOST=Arm80 # Change with your own hostname.
+CLANG_VER="$("$CLANG_ROOTDIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+LLD_VER="$("$CLANG_ROOTDIR"/bin/ld.lld --version | head -n 1)"
+export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+DATE=$(date +"%F-%S")
+TANGGAL=$(date +"%Y%m%d-%H")
+START=$(date +"%s")
+PATH="${PATH}:${CLANG_ROOTDIR}/bin"
 
-IMAGE=$KernelPath/out/arch/arm64/boot/Image.gz
-PATH=${ClangPath}/bin:${PATH}
+# Checking environtment
+# Warning !! Dont Change anything there without known reason.
+function check() {
+echo ================================================
+echo KernelCompiler
+echo version : rev1.5 - gaspoll
+echo ================================================
+echo BUILDER NAME = ${KBUILD_BUILD_USER}
+echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
+echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
+echo TOOLCHAIN_VERSION = ${KBUILD_COMPILER_STRING}
+echo CLANG_ROOTDIR = ${CLANG_ROOTDIR}
+echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
+echo ================================================
+}
 
-
-#
-# Telegram 
-#
+# Telegram
 export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
 
 tg_post_msg() {
@@ -40,83 +64,58 @@ tg_post_msg() {
   -d "disable_web_page_preview=true" \
   -d "parse_mode=html" \
   -d text="$1"
+
 }
 
+# Post Main Information
+tg_post_msg "<b>xKernelCompiler</b>%0ABuilder Name : <code>${KBUILD_BUILD_USER}</code>%0ABuilder Host : <code>${KBUILD_BUILD_HOST}</code>%0ADevice Defconfig: <code>${DEVICE_DEFCONFIG}</code>%0AClang Version : <code>${KBUILD_COMPILER_STRING}</code>%0AClang Rootdir : <code>${CLANG_ROOTDIR}</code>%0AKernel Rootdir : <code>${KERNEL_ROOTDIR}</code>"
 
-#
-# Compile Kernel
-#
-function compile() {
-cd $KernelPath
-tg_post_msg "Nama Kernel:$KERNELNAME""|""User:$KBUILD_BUILD_USER""|""Host:$KBUILD_BUILD_HOST""|""Tanggal:$DATE""|""<b>Sedang MengCompile...🤙</b>" \
-make -j$(nproc) O=out ARCH=arm64 lancelot_defconfig
+# Compile
+compile(){
+tg_post_msg "<b>xKernelCompiler:</b><code>Compilation has started</code>"
+cd ${KERNEL_ROOTDIR}
+make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 make -j$(nproc) ARCH=arm64 O=out \
-    CC=${ClangPath}/bin/clang \
-    CROSS_COMPILE=aarch64-linux-gnu- \
-    CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-    CLANG_TRIPLE=aarch64-linux-gnu-
+    CC=${CLANG_ROOTDIR}/bin/clang \
+    CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
 
    if ! [ -a "$IMAGE" ]; then
 	finerr
-	exit 1
    fi
-   
-git clone --depth=1 https://github.com/CincauEXE/AnyKernel3 -b master AnyKernel
+
+  git clone --depth=1 $ANYKERNEL AnyKernel
 	cp $IMAGE AnyKernel
 }
 
-
-
-#
-# Push Kernel to ch
-#
-function pushdoc() {
+# Push kernel to channel
+function push() {
     cd AnyKernel
     ZIP=$(echo *.zip)
     curl -F document=@$ZIP "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
         -F chat_id="$TG_CHAT_ID" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-        -F caption="VoidKernel"
+        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>$DEVICE_CODENAME</b> | <b>${KBUILD_COMPILER_STRING}</b>"
 }
-
-
-#
-# Linux ver
-#
-function linux(){
-   cd $KernelPath
-   Linux=$(make kernelversion)
-   cd -
-}
-
-
-#
-# Find error
-#
+# Fin Error
 function finerr() {
     curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
         -d chat_id="$TG_CHAT_ID" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=markdown" \
-        -d text="error banh 🗿"
-    exit 1
+        -d text="Build throw an error(s)"
 }
 
-
-#
-# Zipping kernel
-#
+# Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [$DATE][$Version]$KERNELNAME-$Linux-$HASH.zip *
+    zip -r9 $KERNEL_NAME-$CODENAME-${TANGGAL}.zip *
     cd ..
 }
-
-#           #             #             #               #           #
-
-
+check
 compile
-linux
 zipping
-pushdoc
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
