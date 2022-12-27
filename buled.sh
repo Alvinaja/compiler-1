82d2b2c0b6c399dc253d9607eb1fe1b7d98d987e
Selena <87000756+Alvinaja@users.noreply.github.com>
2022-12-27 09:48:21 +0700

    Update buled.sh

--- buled.sh
+++ buled.sh
@@ -1,121 +1,85 @@
-#!/usr/bin/env bash
-#
-# Copyright (C) 2021 a xyzprjkt property
-#
+#!/bin/bash
 
-# Needed Secret Variable
-# KERNEL_NAME | Your kernel name
-# KERNEL_SOURCE | Your kernel link source
-# KERNEL_BRANCH  | Your needed kernel branch if needed with -b. eg -b eleven_eas
-# DEVICE_CODENAME | Your device codename
-# DEVICE_DEFCONFIG | Your device defconfig eg. lavender_defconfig
-# ANYKERNEL | Your Anykernel link repository
-# TG_TOKEN | Your telegram bot token
-# TG_CHAT_ID | Your telegram private ci chat id
-# BUILD_USER | Your username
-# BUILD_HOST | Your hostname
+# vars yourself
+DEFCONFIG=merlin_defconfig
+AK=https://github.com/Alvinaja/AnyKernel3
+ZIPNAME="Kernel-2080"
 
-echo "Downloading few Dependecies . . ."
-# Kernel Sources
-git clone --depth=1 https://github.com/kdrag0n/proton-clang proton # proton set as Clang Default
+# telegram env
+ID=-1001599080496
 
-# Main Declaration
-KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel source root directory.
-DEVICE_DEFCONFIG=lancelot_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
-DEVICE_CODENAME=Lancelot
-KERNEL_NAME=JRE205-ED101
-TG_TOKEN=1689573524:AAHGMEDNkEpxSEc_c_rgwqaQ762n_dWJjJs
-TG_CHAT_ID=1689573524
-CODENAME=lancelot
-ANYKERNEL=https://github.com/01soni247/AnyKernel3
-CLANG_ROOTDIR=$(pwd)/proton # IMPORTANT! Put your clang directory here.
-export KBUILD_BUILD_USER=Soni # Change with your own name or else.
-export KBUILD_BUILD_HOST=Arm80 # Change with your own hostname.
-CLANG_VER="$("$CLANG_ROOTDIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-LLD_VER="$("$CLANG_ROOTDIR"/bin/ld.lld --version | head -n 1)"
-export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
-IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-DATE=$(date +"%F-%S")
+echo "Prepare dependencies"
+mkdir clang-llvm
+mkdir gcc64-aosp
+mkdir gcc32-aosp
+wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r445002.tar.gz "clang-r445002.tar.gz"
+tar -xf clang-r445002.tar.gz -C $(pwd)/clang-llvm
+wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-12.0.0_r32.tar.gz -O "gcc64.tar.gz"
+tar -xf gcc64.tar.gz -C $(pwd)/gcc64-aosp
+wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.0.0_r32.tar.gz -O "gcc32.tar.gz"
+tar -xf gcc32.tar.gz -C $(pwd)/gcc32-aosp
+git clone $AK --depth=1 AnyKernel
+
+echo "Done"
+KERNEL_DIR=$(pwd)
+IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz-dtb"
 TANGGAL=$(date +"%Y%m%d-%H")
-START=$(date +"%s")
-PATH="${PATH}:${CLANG_ROOTDIR}/bin"
+PATH="${KERNEL_DIR}/clang-llvm/bin:${KERNEL_DIR}/gcc64-aosp/bin:${KERNEL_DIR}/gcc32-aosp/bin:${PATH}"
+export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang-llvm/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+export ARCH=arm64
+export KBUILD_BUILD_USER=Alpin
+export KBUILD_BUILD_HOST=Gajelas
 
-# Checking environtment
-# Warning !! Dont Change anything there without known reason.
-function check() {
-echo ================================================
-echo KernelCompiler
-echo version : rev1.5 - gaspoll
-echo ================================================
-echo BUILDER NAME = ${KBUILD_BUILD_USER}
-echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
-echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
-echo TOOLCHAIN_VERSION = ${KBUILD_COMPILER_STRING}
-echo CLANG_ROOTDIR = ${CLANG_ROOTDIR}
-echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
-echo ================================================
-}
+make O=out ARCH=arm64 $DEFCONFIG
 
-# Telegram
-export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+# Compile plox
+function compile() {
+    make -j$(nproc --all) O=out ARCH=arm64 CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
 
-tg_post_msg() {
-  curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
-  -d "disable_web_page_preview=true" \
-  -d "parse_mode=html" \
-  -d text="$1"
-
-}
-
-# Post Main Information
-tg_post_msg "<b>xKernelCompiler</b>%0ABuilder Name : <code>${KBUILD_BUILD_USER}</code>%0ABuilder Host : <code>${KBUILD_BUILD_HOST}</code>%0ADevice Defconfig: <code>${DEVICE_DEFCONFIG}</code>%0AClang Version : <code>${KBUILD_COMPILER_STRING}</code>%0AClang Rootdir : <code>${CLANG_ROOTDIR}</code>%0AKernel Rootdir : <code>${KERNEL_ROOTDIR}</code>"
-
-# Compile
-compile(){
-tg_post_msg "<b>xKernelCompiler:</b><code>Compilation has started</code>"
-cd ${KERNEL_ROOTDIR}
-make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
-make -j$(nproc) ARCH=arm64 O=out \
-    CC=${CLANG_ROOTDIR}/bin/clang \
-    CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
-    CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
-
-   if ! [ -a "$IMAGE" ]; then
-	finerr
-   fi
-
-  git clone --depth=1 $ANYKERNEL AnyKernel
-	cp $IMAGE AnyKernel
-}
-
-# Push kernel to channel
-function push() {
-    cd AnyKernel
-    ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
-        -F chat_id="$TG_CHAT_ID" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>$DEVICE_CODENAME</b> | <b>${KBUILD_COMPILER_STRING}</b>"
-}
-# Fin Error
-function finerr() {
-    curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
-        -d chat_id="$TG_CHAT_ID" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=markdown" \
-        -d text="Build throw an error(s)"
+    if ! [ -a "$IMAGE" ]; then
+        finerr
+        exit 1
+    fi
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 $KERNEL_NAME-$CODENAME-${TANGGAL}.zip *
+    zip -r9 $ZIPNAME-${TANGGAL}.zip *
     cd ..
 }
-check
+
+function start() {
+    curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
+        -d chat_id="$ID" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="Build started on $(date) with $KBUILD_COMPILER_STRING"
+}
+# Push kernel to channel
+
+function push() {
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$TOKEN/sendDocument" \
+        -F chat_id="$ID" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="OSS Kernel | Build finished on $(date)"
+}
+# Fin Error
+
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
+        -d chat_id="$ID" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
+}
+
+start
 compile
 zipping
-END=$(date +"%s")
-DIFF=$(($END - $START))
 push
